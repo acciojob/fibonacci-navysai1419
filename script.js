f68e88262999fbb2dc5abc1db92b317ffc4939eb
Navya <88023981+navysai1419@users.noreply.github.com>
2023-12-08 14:42:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function fibonacci(num) {
-// your code here
+  const fibonacciSequence = [0, 1];
+
+  for (let i = 2; i < num; i++) {
+    const nextFibonacci = fibonacciSequence[i - 1] + fibonacciSequence[i - 2];
+    fibonacciSequence.push(nextFibonacci);
+  }
+
+  return fibonacciSequence.slice(0, num);
 }
 
 module.exports = fibonacci;
